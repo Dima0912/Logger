2a530d5ee74269012efb1fc43f7360668b6ef918
give your user name here <your.email.id>
2022-02-05 13:16:16 +0200

    logger

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'vendor/autoload.php';
+
+
+use \App\Logger;
+use \App\StringFormat;
+use \App\FileWriter;
+use \App\DbWriter;
+use Psr\Log\AbstractLogger;
+
+
+$fileWriter = new FileWriter;
+$format = new StringFormat;
+
+$dbWriter = new DbWriter();
+
+
+$logger = new Logger($fileWriter, $format);
+$logger2 = new Logger($dbWriter, $format);
+
+
+function testLogger(AbstractLogger $logger)
+{
+    $logger->doSomething();
+}
+
+testLogger($logger);
+testLogger($logger2);
